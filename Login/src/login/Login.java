79992c00014d0c0f07a37fc2a099fb0044ae0b43
Pures6322 <Pures6322@HRH-IC0037410.wrdsb.ca>
2019-12-20 10:45:07 -0500

    Login Setup

--- Login/src/login/Login.java
+++ Login/src/login/Login.java
@@ -5,6 +5,10 @@
  */
 package login;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
 /**
  *
  * @author Pures6322
@@ -14,8 +18,11 @@ public class Login {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        File file = new File("X:\\vlcplayer\\vlc.exe");
+        Desktop.getDesktop().open(file);
+        System.out.println("Worked!");
+        
     }
     
 }
